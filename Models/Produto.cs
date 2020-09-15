7a821be6122af80670b798d0bc21d5819c654547
Maria Andreia de Araújo Pereira <mariaandry@hotmail.com>
2020-09-14 23:00:01 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp1.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public String Nome { get; set; }
+
+        public int Entrada { get; set; }
+
+        public int Vencimento { get; set;}
+
+    public string Descrição  { get; set;}
+
+public string Status { get; set; }
+
+    }
+}
